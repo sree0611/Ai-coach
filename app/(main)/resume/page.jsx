45c3cf03f0286984c2091d8794a444a73ca2dd8a
Sujith <sujith.m1524@gmail.com>
2025-02-16 18:58:47 +0530

    first

--- app/(main)/resume/page.jsx
+++ app/(main)/resume/page.jsx
@@ -4,6 +4,8 @@ import ResumeBuilder from "./_components/resume-builder";
 export default async function ResumePage() {
   const resume = await getResume();
 
+   console.log("bdfkghdkfh");
+   
   return (
     <div className="container mx-auto py-6 px-8">
       <ResumeBuilder initialContent={resume?.content} />
